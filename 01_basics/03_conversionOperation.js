9c7aecf70f1e546c30a27a90c91efbf4cee142e8
Sunny Kumar Yadav <mailsunny333@gmail.com>
2024-03-23 22:14:18 +0530

    Conversion completed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -31,4 +31,36 @@ let booleamIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33;
 let stringNumber = String(someNumber)
-console.log(stringNumber)
+// console.log(stringNumber)
+
+// ---------------------------- OPERATIONS -------------------------------------
+
+let val = 3
+let negVal = -val
+// console.log(negVal)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+
+let str1 = "hello"
+let str2 = " ram"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(typeof ("1" + 2))
+// console.log(1 + "2")
+
+// console.log("1" + 2 + 2)    //priority to string
+// console.log(1 + 2 + "2")    //priority to int
+
+
+// console.log(true)
+// console.log(+true)  //=>conversion to int
+// console.log(+"")    //=>conversion to int
+
+
